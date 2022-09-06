ebce72206fdfc0c549f6c35cbaab7f69532df951
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-09-06 16:09:15 -0300

    estilização

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Novo Usuário')
 
 @section('content')
-<h1>Novo Usuário</h1>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Novo Usuário</h1>
 
 @include('includes.validations-form')
 
-<form action="{{ route('users.store') }}" method="POST">
+<form action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data">
     @include('users._partials.form')
 </form>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,19 +3,40 @@
 @section('title', 'Usuário')
 
 @section('content')
-<h1>Listagem do usuário {{ $user->name }}</h1>
-
-<ul>
-    <li>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Listagem do usuário {{ $user->name }}</h1>
+<table class="min-w-full leading-normal shadow-md rounded-lg overflow-hidden">
+    <thead>
+        <tr>
+          <th
+            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+            Nome
+          </th>
+          <th
+            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+            E-mail
+          </th>
+          <th
+            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+            <form action="{{ route('users.delete', $user->id) }}" method="POST">
+                @method('DELETE')
+                @csrf
+                <button type="submit"  class="rounded-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">Deletar</button>
+            </form>
+          </th>
+        </tr>
+    </thead>
+    <tbody>
+<tr>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         {{ $user->name }}
-    </li>
-    <li>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         {{ $user->email }}
-    </li>
-</ul>
-<form action="{{ route('users.delete', $user->id) }}" method="POST">
-    @method('DELETE')
-    @csrf
-    <button type="submit">Deletar</button>
-</form>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+    </td>
+</tr>
+</tbody>
+
+
 @endsection
