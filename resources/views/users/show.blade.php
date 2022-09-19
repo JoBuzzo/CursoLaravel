7ece44499f32532e3752627d2f296d1fd1adad0f
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-09-19 11:21:29 -0300

    Melhoria do design da aba detalhes

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,14 +8,14 @@
 </div>
 <h1 class="text-4xl font-semibold leading-tigh py-2 ">Listagem do usuário {{ $user->name }}</h1>
 
-<div class="w-full bg-white shadow-md rounded px-8 py-30">
+<div class="w-full shadow-md rounded px-8 py-30">
     <ul class="py-12 px-80">
-        <li class="rounded-full py-2 px-0 text-center border-2 border-gray-200">{{ $user->name }}</li>
+        <li class="rounded-full py-2 text-center border-2 border-gray-200">{{ $user->name }}</li>
         <br>
-        <li class="rounded-full py-2 px-0 text-center border-2 border-gray-200">{{ $user->email }}</li>
+        <li class="rounded-full py-2 text-center border-2 border-gray-200">{{ $user->email }}</li>
     </ul>
 
-<form action="{{ route('users.delete', $user->id) }}" method="POST" class="py-12 text-center">
+<form action="{{ route('users.delete', $user->id) }}" method="POST" class="py-5  text-center">
     @method('DELETE')
     @csrf
     <button type="submit" class="rounded-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 ">Deletar</button>
